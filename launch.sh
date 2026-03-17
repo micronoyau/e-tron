e1cf8123deff187a366c3d6917ae9d8b2e08f4c2
micronoyau <leo.lf.dk@gmail.com>
2026-03-17 21:12:18 +0100

    feat: History saved

--- launch.sh
+++ launch.sh
@@ -1,18 +1,19 @@
 PROJECT_UID=$(pwd -P | sha256sum | awk '{print $1}')
-OPENCODE_SHARE_DIR=$HOME/.local/share/exploitron/$PROJECT_UID
+EXPLOITRON_SESSION_DIR=$HOME/.local/share/exploitron/$PROJECT_UID
 OPENCODE_CREDS=$HOME/.local/share/opencode/auth.json
 
-if [ ! -d $OPENCODE_SHARE_DIR ]; then
-  echo "Creating opencode share dir in $OPENCODE_SHARE_DIR ..."
-  mkdir -p $OPENCODE_SHARE_DIR
-  cp $OPENCODE_CREDS $OPENCODE_SHARE_DIR
+if [ ! -d $EXPLOITRON_SESSION_DIR ]; then
+  echo "Creating exploitron session in $EXPLOITRON_SESSION_DIR ..."
+  mkdir -p $EXPLOITRON_SESSION_DIR/opencode $EXPLOITRON_SESSION_DIR/local
+  cp $OPENCODE_CREDS $EXPLOITRON_SESSION_DIR/opencode
 fi
 
 docker run \
   --rm \
   -it \
   --mount type=bind,source="$(pwd)",target="/home/user/chal" \
-  --mount type=bind,source="$OPENCODE_SHARE_DIR",target="/home/user/.local/share/opencode/" \
+  --mount type=bind,source="$EXPLOITRON_SESSION_DIR/local",target="/home/user/.local/share" \
+  --mount type=bind,source="$EXPLOITRON_SESSION_DIR/opencode",target="/home/user/.local/share/opencode" \
   --mount type=bind,source="${HOME}/Downloads",target="/root/Downloads" \
   --mount type=bind,source="${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY}",target="/tmp/${WAYLAND_DISPLAY}" \
   -e XDG_RUNTIME_DIR=/tmp \
